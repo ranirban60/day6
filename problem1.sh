33e0d1339e7baa26939ea639e5acca6f5a215ea5
ranirban60@gmail.com <ranirban60@gmail.com>
2022-06-08 08:12:55 +0530

    File is Added

--- problem1.sh
+++ problem1.sh
@@ -1,12 +1,24 @@
-n='^[0-9]+$'
-for i in "$@"
+#n='^[0-9]+$'
+#for i in "$@"
+#do
+#if
+#[[ $i =~ $n ]];
+#then
+#if [[ $((i & (i-1))) == 0 ]];
+#then
+#echo " $i is a power of 2 "
+#fi
+#fi
+#done
+c=0
+i=1
+echo "Enter an integer for the base"
+read $m
+echo "Enter a positive integer for the power"
+read $n
+while (( i > 0 ))
 do
-if
-[[ $i =~ $n ]];
-then
-if [[ $((i & (i-1))) == 0 ]];
-then
-echo " $i is a power of 2 "
-fi
-fi
+  c=$(($m**$n))
 done
+echo "The product of the numbers is: $c"
+
